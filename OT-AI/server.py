ccd5cf44b883e860578f59f4e729c0c47ec2279c
neotje <neotje111@gmail.com>
2020-09-18 16:09:28 +0200

    async stuff

--- OT-AI/server.py
+++ OT-AI/server.py
@@ -1,13 +1,16 @@
 import streaming
 import cv2
 import linedetection
-
-server = streaming.Server(('192.168.111.106', 8084))
+from streaming import AsyncServer
+import asyncio
 
 cv2.namedWindow('frame')
 cv2.startWindowThread()
 
 steer = 0
+
+"""
+server = streaming.Server(('192.168.111.106', 8084))
 
 while True:
     server.wait_for_connection()
@@ -18,14 +21,27 @@ while True:
 
         if type(frame) == bool:
             break
-        
+
         lanes = linedetection.detect_lane(frame)
-        steer = linedetection.stabilize_steering_angle(linedetection.compute_steering_angle(frame, lanes), steer, len(lanes))
+        steer = linedetection.stabilize_steering_angle(
+            linedetection.compute_steering_angle(frame, lanes), steer, len(lanes))
         frame = linedetection.display_lines(frame, lanes)
         frame = linedetection.display_heading_line(frame, steer)
         cv2.imshow('frame', frame)
         cv2.waitKey(1)
 
     print("client disconnected")
-    #cv2.destroyAllWindows()
-    
+    # cv2.destroyAllWindows()
+"""
+server = AsyncServer('127.0.0.1', 8084)
+
+@server.on_frame()
+def frame_handler(frame):
+    lanes = linedetection.detect_lane(frame)
+    steer = linedetection.stabilize_steering_angle(linedetection.compute_steering_angle(frame, lanes), steer, len(lanes))
+    frame = linedetection.display_lines(frame, lanes)
+    frame = linedetection.display_heading_line(frame, steer)
+    cv2.imshow('frame', frame)
+    cv2.waitKey(1)
+
+asyncio.run(server.serve())
--- OT-AI/streaming.py
+++ OT-AI/streaming.py
@@ -46,6 +46,51 @@ class Server():
 
         return frame
 
+class AsyncServer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.on_frame_array = []
 
+        self.package_size = struct.calcsize('L')
+        self.data = b''
 
+    async def serve(self):
+        self.sock = await asyncio.start_server(self.server_handler, self.host, self.port)
+        await self.sock.serve_forever()
 
+    async def server_handler(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        while True:
+            while(len(self.data) < self.package_size):
+                buf = await reader.read(buffer_size)
+                if len(buf) == 0:
+                    continue
+                self.data += buf
+            packed_msg_size = self.data[:self.package_size]
+
+            # unpack data
+            self.data = self.data[self.package_size:]
+            msg_size = struct.unpack("L", packed_msg_size)[0]
+
+            # recieve frame data
+            while(len(self.data) < msg_size):
+                buf = await reader.read(buffer_size)
+                if len(buf) == 0:
+                    continue
+                self.data += buf
+            frame_data = self.data[:msg_size]
+            self.data = self.data[msg_size:]
+
+            frame = pickle.loads(frame_data)
+            self.call_on_frame(frame)
+
+    
+    def call_on_frame(self, frame):
+        for func in self.on_frame_array:
+            func(frame)
+
+    def on_frame(self):
+        def decorator(f):
+            self.on_frame_array.append(f)
+            return f
+        return decorator
