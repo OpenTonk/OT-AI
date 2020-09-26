d44b85a8fe0042c629fa06ec6385e1978ac1e6fc
neotje <neotje111@gmail.com>
2020-09-26 10:16:44 +0200

    added client cmd args

--- OT-AI/client.py
+++ OT-AI/client.py
@@ -3,38 +3,44 @@ import asyncio
 from streaming import AsyncClient
 import comms
 import threading
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
+import sys, getopt
 
-isPi = False
 
-# cap = cv2.VideoCapture('video.mp4')
-if isPi:
-    cam = PiCamera()
-    #cam.resolution = (640, 480)
-    #cam.framerate = 30
-    rawCapture = PiRGBArray(cam)
-
-else:
-    cap = cv2.VideoCapture(-1)
-
-client = AsyncClient('192.168.111.106', 8084)
+ip = "127.0.0.1"
+port = 8084
+usePiCam = False
 size = 1
 
-comms = comms.AsyncClient('192.168.111.106', 8085)
 
-time.sleep(0.1)
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "ha:p:s:", ["ip=", "port=", "size=", "usepicam"])
+except getopt.GetoptError:
+    print("client.py -a <server ip> -p <port> -s <size> (-usepicam)")
+    exit()
 
+for opt, arg in opts:
+    if opt == '-h':
+        print("client.py -a <server ip> -p <port> -s <size> (-usepicam)")
+        exit()
+    elif opt in ("-a", "-ip"):
+        ip = arg
+    elif opt in ("-p", "-port"):
+        port = int(arg)
+    elif opt in ("-s", "-size"):
+        size = int(arg)
+    elif opt == '-usepicam':
+        usePiCam = True
+
+
+cap = cv2.VideoCapture(-1)
+
+client = AsyncClient(ip, port, usePiCam)
+
+comms = comms.AsyncClient(ip, port + 1)
 
 @client.on_get_frame()
 def read_frame():
-    if isPi:
-        ret = True
-        cam.capture(rawCapture, format="bgr")
-        frame = rawCapture.array
-    else:
-        ret, frame = cap.read()
+    ret, frame = cap.read()
 
     if ret:
         h, w, _ = frame.shape
