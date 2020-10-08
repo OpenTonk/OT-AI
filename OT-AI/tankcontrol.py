4ffb8838c895c41fb414f751f0cf2439fe132dd4
neotje <neotje111@gmail.com>
2020-10-08 15:03:43 +0200

    added pwm to tankcontroller

--- OT-AI/tankcontrol.py
+++ OT-AI/tankcontrol.py
@@ -1,10 +1,31 @@
+import RPi.GPIO as IO
+
+IO.setwarning(False)
+IO.setmode(IO.BCM)
+
 class TankControl:
     def __init__(self, right: tuple, left: tuple):
-        self.right = right
-        self.left = left
+        IO.setup(right[0], IO.IN)
+        IO.setup(right[1], IO.IN)
+        IO.setup(left[0], IO.IN)
+        IO.setup(left[1], IO.IN)
+        
+        self.right = (
+            IO.PWM(right[0], 100), 
+            IO.PWM(right[1], 100)
+        )
+        self.left = (
+            IO.PWM(left[0], 100), 
+            IO.PWM(left[1], 100)
+        )
 
         self.rightSpeed = 0
         self.leftSpeed = 0
+
+        self.right[0].start(0)
+        self.right[1].start(0)
+        self.left[0].start(0)
+        self.left[1].start(0)
 
     def drive(self, speed: int, angle: int):
         if angle > 90:
@@ -23,5 +44,25 @@ class TankControl:
     def setRightSpeed(self, speed: int):
         self.rightSpeed = speed
 
+        if speed > 0:
+            self.right[0].ChangeDutyCycle(speed)
+            self.right[1].ChangeDutyCycle(0)
+        elif speed == 0:
+            self.right[0].ChangeDutyCycle(0)
+            self.right[1].ChangeDutyCycle(0)
+        else:
+            self.right[0].ChangeDutyCycle(0)
+            self.right[1].ChangeDutyCycle(speed)
+
     def setLeftSpeed(self, speed: int):
         self.leftSpeed = speed
+
+        if speed > 0:
+            self.left[0].ChangeDutyCycle(speed)
+            self.left[1].ChangeDutyCycle(0)
+        elif speed == 0:
+            self.left[0].ChangeDutyCycle(0)
+            self.left[1].ChangeDutyCycle(0)
+        else:
+            self.left[0].ChangeDutyCycle(0)
+            self.left[1].ChangeDutyCycle(speed)
